feff7abb8261d07717cda3a6568b8d8e96a19e41
unknown <cortezlisi202@gmail.com>
2026-05-17 17:33:48 -0600

    CASI LISTA INTERFAZ

--- AventuraEspacial_Fer_Lisi_Edu/AventuraEspacial_Fer_Lisi_Edu/Form1.cs
+++ AventuraEspacial_Fer_Lisi_Edu/AventuraEspacial_Fer_Lisi_Edu/Form1.cs
@@ -21,5 +21,20 @@ namespace AventuraEspacial_Fer_Lisi_Edu
         {
 
         }
+
+        private void AventuraEspacial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
